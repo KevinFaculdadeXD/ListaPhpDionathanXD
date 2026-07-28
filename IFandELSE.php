28b8860203f4eb7976f401a497e5aa18aa0a1a27
KevinR_XD69 <engs-kevinrichardt@camporeal.edu.br>
2026-07-27 22:02:42 -0300

    atividade 1 feita

---
+++ IFandELSE.php
@@ -0,0 +1,14 @@
+<?php
+// 1. Crie uma variável $idade e mostre se é maior ou menor de idade
+$idadeManeira = 18;
+
+if ($idadeManeira >= 18) {
+    echo"você pode jogar roblox :)";
+} else {
+    echo "você não pode jogar roblox :(";
+}
+echo"\n----------------";
+echo "\n";
+
+?>
+
