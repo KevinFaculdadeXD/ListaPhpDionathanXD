e57ede093f2187be3009022bfa1e20c629652bb9
Kevin Richardt <engs-kevinrichardt@camporeal.edu.br>
2026-07-30 01:12:18 -0300

    Exercicio 9 feito

--- array.php
+++ array.php
@@ -16,7 +16,21 @@ foreach ($AlbumManeiroQueEscutavaEnquantoFaziaEssaAtividade as $BrasilianSkies){
     echo $frasesSuperFodas . "\n";
 }
 
+echo"\n----------------";
+echo "\n";
 // Exercicio 9
 
+$racasPapagaioSuperFodas = [
+    "Papagaio-verdadeiro",
+    "Papagaio-do-mangue",
+    "Papagaio-de-bochecha-azul "
+];
 
+// Adicionando mais uma raça
+$racasPapagaioSuperFodas[] = "Papagaio-moleiro";
+
+print_r($racasPapagaioSuperFodas);
+
+echo"\n----------------";
+echo "\n";
 ?>
