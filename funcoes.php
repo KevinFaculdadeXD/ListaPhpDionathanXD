9621a59e1925cf4133465bfc67fadd7558108965
Kevin Richardt <engs-kevinrichardt@camporeal.edu.br>
2026-07-29 23:57:50 -0300

    Exercicio 6 e 7 pronto (muita aura)

---
+++ funcoes.php
@@ -0,0 +1,29 @@
+//Exercicio 6
+<?php
+function saudacao($nomeDaora) {
+    echo"Olá $nomeDaora!\n";
+}
+
+saudacao("John Marston");
+
+echo"\n----------------";
+echo "\n";
+//Exercicio 7
+
+function calcularEFrase($num1, $num2){
+    $soma = $num1 + $num2;
+    $subtracao = $num1 - $num2;
+    $fraseXD = "A Soma tem muita aura uff (emoji de fogo)";
+
+    return [
+        'soma' => $soma,
+        'subtracao' => $subtracao,
+        'fraseXD' => $fraseXD
+    ];
+}
+
+$resultado = calcularEFrase(60, 7);
+echo "Soma: " . $resultado['soma'] . "\n";
+echo "Subtração: " . $resultado['subtracao'] . "\n";
+echo "Frase: " . $resultado['fraseXD'] . "\n";
+?>
