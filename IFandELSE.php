19ec6aef628bb8982759764718c16fe7bf029afc
Kevin Richardt <engs-kevinrichardt@camporeal.edu.br>
2026-07-29 22:35:57 -0300

    Atividade 2 feita

--- IFandELSE.php
+++ IFandELSE.php
@@ -10,5 +10,32 @@ if ($idadeManeira >= 18) {
 echo"\n----------------";
 echo "\n";
 
+/*2. Crie uma variável $dinheiro.
+
+Exiba:
+Pobre
+Classe Média
+Riquinho
+Ricão
+Elon Musk */
+
+$dinheiro = 500000000.00;
+
+if ($dinheiro < 300) {
+    echo "Pobre de m\n";
+} elseif ($dinheiro < 1000) {
+    echo "Classe Média\n";
+} elseif ($dinheiro < 10000) {
+    echo "Riquinho\n";
+} elseif ($dinheiro < 100000) {
+    echo "Podre de rico(Ou Ricão)\n";
+} else {
+    echo "Elon Musk\n";
+}
+
+echo"\n----------------";
+echo "\n";
+
+
 ?>
 
