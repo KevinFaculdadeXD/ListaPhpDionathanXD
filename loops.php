5ae4aa64a80c7f64b32a5b192b88208ce749ae8f
Kevin Richardt <engs-kevinrichardt@camporeal.edu.br>
2026-07-29 23:38:23 -0300

    Exercicio 5 feito

--- loops.php
+++ loops.php
@@ -8,4 +8,18 @@ for ($i = 1; $i <= 100; $i++) {
 }
 echo"\n----------------";
 echo "\n";
+
+// Exercicio 5
+
+$numerosManeiros = [4, 7, 12879.5];
+
+foreach ($numerosManeiros as $num) {
+    echo "---Tabuada do $num---\n";
+        for ($i = 1; $i <= 10; $i++) {
+            $resultadoDaora = $num * $i;
+            echo "$num x $i = " . number_format($resultadoDaora,2, ',','.') ."\n";
+        }
+        echo"\n----------------";
+echo "\n";
+}
 ?>
